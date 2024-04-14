002b1618342d8bcc7d9fcaf00a54233a97861407
Erin Kelley <erin.kelley01@gmail.com>
2024-04-14 12:05:00 -0600

    Add fetch request to get specific park

--- src/ParkDetails.js
+++ src/ParkDetails.js
@@ -1,9 +1,33 @@
 import './ParkDetails';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export default function ParkDetails(){
+    const parkName = useParams().park
+    const [parks, setParks] = useState([])
+
+    const parkCodes = ['acad', 'arch', 'badl', 'bibe', 'bisc', 'blca', 'brca', 'cany', 'care', 'cave', 'chis', 'cong', 
+    'crla', 'cuva', 'deva', 'drto', 'dena', 'ever', 'jeff', 'gaar', 'glac', 'glba', 'grca', 'grte', 'grba', 'grsa', 'grsm', 
+    'gumo', 'hale', 'havo', 'hosp', 'indu', 'isro', 'jotr', 'katm', 'kefj', 'kova', 'lacl', 'lavo', 'maca', 'meve', 
+    'mora', 'neri', 'noca', 'npsa', 'olym', 'pefo', 'pinn', 'redw', 'romo', 'sagu', 'seki', 'shen', 'thro', 'viis', 'voya', 'whsa', 'wica', 'wrst',
+    'yell', 'yose', 'zion']  
+    
+    useEffect(() => {
+         fetch(`https://developer.nps.gov/api/v1/parks/?api_key=L85BGF7lUDFAWRO9q3sz5DijJohpytxVfajv4UXu&parkCode=${parkCodes}&limit=62`)
+
+    .then(res => res.json())
+    .then(data => setParks(findPark(data.data)))
+    }, [])
+    console.log(parks[0].fullName)
+   
+    function findPark(allParks){
+        const specificPark = allParks.filter(park => park.fullName === parkName)
+        return specificPark; 
+    }
+
     return (
         <div>
-            <h1>Park Details Lie Here.</h1>
+            <h1>{parks[0].fullName}</h1>
         </div>
     )
 }
--- src/RegionParks.js
+++ src/RegionParks.js
@@ -7,16 +7,7 @@ import { useState, useEffect } from 'react';
 
 export default function RegionParks(){
     const [states, setStates] = useState([])
-    // const parkCodes = ['acad', 'arch', 'badl', 'bibe', 'bisc', 'blca', 'brca', 'cany', 'care', 'cave', 'chis', 'cong', 
-    // 'crla', 'cuva', 'deva', 'drto', 'dena', 'ever', 'jeff', 'gaar', 'glac', 'glba', 'grca', 'grte', 'grba', 'grsa', 'grsm', 
-    // 'gumo', 'hale', 'havo', 'hosp', 'indu', 'isro', 'jotr', 'katm', 'kefj', 'kova', 'lacl', 'lavo', 'maca', 'meve', 
-    // 'mora', 'neri', 'noca', 'npsa', 'olym', 'pefo', 'pinn', 'redw', 'romo', 'sagu', 'seki', 'shen', 'thro', 'viis', 'voya', 'whsa', 'wica', 'wrst',
-    // 'yell', 'yose', 'zion']  
-    
-    // fetch(`https://developer.nps.gov/api/v1/parks/?api_key=L85BGF7lUDFAWRO9q3sz5DijJohpytxVfajv4UXu&parkCode=${parkCodes}&limit=62`)
-    // // fetch('https://developer.nps.gov/api/v1/parks/?api_key=L85BGF7lUDFAWRO9q3sz5DijJohpytxVfajv4UXu&start=150')
-    // .then(res => res.json())
-    // .then(data => console.log(data))
+
     
     const area = useParams().region
     const parkByRegion = locations.filter(location => {
