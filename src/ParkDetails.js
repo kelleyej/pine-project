f227ecafe968c3f6db58eea6e5559aea71574645
Erin Kelley <erin.kelley01@gmail.com>
2024-04-15 11:50:46 -0600

    Add phone number details

--- src/ParkDetails.js
+++ src/ParkDetails.js
@@ -25,7 +25,7 @@ export default function ParkDetails(){
         const specificPark = allParks.filter(park => park.fullName === parkName)
         return specificPark; 
     }
-    console.log(parks[0].addresses)
+    console.log(parks[0].contacts.phoneNumbers[0].phoneNumber)
 
     if(!parks.length){
         return (
@@ -39,6 +39,7 @@ export default function ParkDetails(){
                 <p>{parks[0].directionsInfo}</p>
                 <a href={parks[0].directionsUrl}>Link to Directions</a>
                 <a href={parks[0].url}>Link to Official Site</a>
+                <p>Phone Number: {parks[0].contacts.phoneNumbers[0].phoneNumber} ({parks[0].contacts.phoneNumbers[0].type})</p>
                 {/* <p>{parks[0].entranceFees}</p> */}
                 {/* <img src={parks[0].images[0].url}/> */}
             </section>
