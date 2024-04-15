ed6e19feed65bdd48f61debdde9a284f63de166b
Erin Kelley <erin.kelley01@gmail.com>
2024-04-15 12:58:02 -0600

    Add weekly operating hours

--- src/OperatingHours.js
+++ src/OperatingHours.js
@@ -1,8 +1,19 @@
+import OperatingHoursCard from "./OperatingHoursCard";
 
-export default function OperatingHours() {
+export default function OperatingHours({parks}) {
+
+    const hours = parks[0].operatingHours.map(hour => {
+         return (
+       <OperatingHoursCard
+       description={hour.description}
+       standardHours={hour.standardHours}
+       />
+    )
+    })
     return (
         <div>
-            
+            {hours}
         </div>
     )
+   
 }
