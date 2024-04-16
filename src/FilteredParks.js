8c89b474b114cc8201054f0ce70bc053b643ca8f
Erin Kelley <erin.kelley01@gmail.com>
2024-04-15 18:34:35 -0600

    Add link to filtered parks

--- src/FilteredParks.js
+++ src/FilteredParks.js
@@ -1,16 +1,19 @@
 import FilteredParksCard from './FilteredParksCard';
 import './FilteredParks.css';
+import { Link } from 'react-router-dom';
 
 export default function FilteredParks({test}){
 
 const parkByState = test.map(t => {
    return (
+    <Link to={`/region/${t.name}`}>
         <FilteredParksCard
             name={t.name}
             city={t.city}
             state={t.state}
             image={t.image}
        />
+       </Link>
     )
 
 })
