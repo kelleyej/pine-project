da046e5da49d5264f0e3f1b710ed4ca1e6291ee1
Erin Kelley <erin.kelley01@gmail.com>
2024-04-15 21:33:25 -0600

    Add state styling

--- src/StateCard.js
+++ src/StateCard.js
@@ -1,13 +1,11 @@
 import './StateCard.css';
-import { useState } from 'react';
 
-
-export default function StateCard({location, parkByRegion, filterParks}){
+export default function StateCard({location, filterParks}){
 
     return (     
             <div className='state-container' onClick={event => filterParks(event.target.id)}>
-            <h3 id={location} className='state'>{location}</h3>
-            
+            <h3 id={location} className='state'>{location} </h3>
+            <img className='pine' src="https://img.freepik.com/premium-vector/vintage-pine-tree-illustration_722738-97.jpg"/>
         </div>  
 
     )
