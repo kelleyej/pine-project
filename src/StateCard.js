628c41aacf7f23ad16f8d1456a976536e10ed198
Erin Kelley <erin.kelley01@gmail.com>
2024-04-16 08:06:06 -0600

    Make tree image clickable for state

--- src/StateCard.js
+++ src/StateCard.js
@@ -5,7 +5,7 @@ export default function StateCard({location, filterParks}){
     return (     
             <div className='state-container' onClick={event => filterParks(event.target.id)}>
             <h3 id={location} className='state'>{location} </h3>
-            <img className='pine' src="https://img.freepik.com/premium-vector/vintage-pine-tree-illustration_722738-97.jpg"/>
+            <img id={location} className='pine' src="https://img.freepik.com/premium-vector/vintage-pine-tree-illustration_722738-97.jpg"/>
         </div>  
 
     )
