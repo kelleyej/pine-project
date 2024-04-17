9146b65a310632e189ca31deb2a4a2bdf4f10308
Erin Kelley <erin.kelley01@gmail.com>
2024-04-17 11:31:01 -0600

    Add state to track checked radio buttons

--- src/components/ParkTracker/ParkTracker.js
+++ src/components/ParkTracker/ParkTracker.js
@@ -1,23 +1,50 @@
-import locations from '../LocationInfo/LocationInfo';
+
 import ParkTrackerCard from '../ParkTrackerCard/ParkTrackerCard';
 import './ParkTracker.css';
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function ParkTracker({parks}){
-console.log('PARK TRACKERRRRRR:', parks)
-        const park = locations.map(area => {
+
+    const [checked, setChecked] = useState(false)
+const [checkedState, setCheckedState] = useState(new Array(parks.length).fill(false))
+const [count, setCount] = useState(0)
+
+
+    const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+  
+    setCheckedState(updatedCheckedState);
+}
+
+let number = checkedState.filter(state => state === true)
+console.log(number.length)
+        const park = parks.map((area, index) => {
             return (
-                 <div>
-          
+       
+                  
+        <div className='park-container'>
+            <p className='park-tracker-name'>{area.name}</p>
+            <img className='tracker-image' src="https://cdn.pixabay.com/photo/2012/04/13/11/16/pine-31913_640.png"/> 
+            <input id={area.id} name={area.name} type='checkbox' checked={checkedState[index]} onChange={() => handleOnChange(index)} />
+        </div>
+ 
+/*           
             <ParkTrackerCard 
                 name={area.name}
                 key={area.id}
                 id={area.id}
                 visited={area.visited}
-            />  
-        </div>   
+                parks={parks}
+                area={area}
+                index={index}
+            />   */
+      
             )
         })
+
+console.log(checkedState)
 
     return (
         <div className='park-tracker-container'>
--- src/components/ParkTrackerCard/ParkTrackerCard.js
+++ src/components/ParkTrackerCard/ParkTrackerCard.js
@@ -1,35 +1,57 @@
-import './ParkTrackerCard.css';
-import { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+// import './ParkTrackerCard.css';
+// import { useState, useEffect, prevState } from 'react';
+// import PropTypes from 'prop-types';
 
-export default function ParkTrackerCard({name, id}){
+// export default function ParkTrackerCard({name, id, visited, parks, area, index}){
+// // const [allParks, setAllParks] = useState([])
+// // const [checked, setChecked] = useState(false)
+// // const [checkedState, setCheckedState] = useState(new Array(parks.length).fill(false))
 
-const [checked, setChecked] = useState(false)
 
-    if(checked){
+//       console.log(checkedState)
+//     //  if(checked){
+//     //     console.log(allParks)
+//     //     let test = parks.filter(park => park.id === id)
+//     //     setAllParks([test])
+//     // } else {
+//     //     console.log('not working')
+//     // }
 
-         return (
-        <div className='park-container'>
-            <p className='park-tracker-name'>{name}</p>
-            <img className='tracker-image' src="https://cdn.pixabay.com/photo/2012/04/13/11/16/pine-31913_640.png"/> 
-            <input id={id} name={name} type='checkbox' onClick={event => setChecked(event.target.checked)} />
-        </div>
-    )
 
-    } else {
 
-        return (
-            <div className='park-container'>
-            <p>{name}</p>
-            <img className='tracker-image' src="https://i.pinimg.com/564x/c1/07/12/c1071269b82e857c39c5ef6ac265d7f4.jpg"/> 
-            <input id={id} type='checkbox' onClick={event => setChecked(event.target.checked)} />
-        </div>
-        )
-    }
+//     const handleOnChange = (position) => {
+//         const updatedCheckedState = checkedState.map((item, index) =>
+//           index === position ? !item : item
+//         );
+      
+//         setCheckedState(updatedCheckedState);
+      
+//     }
+
+//     // if(checked){
+
+//          return (
+//         <div className='park-container'>
+//             <p className='park-tracker-name'>{name}</p>
+//             <img className='tracker-image' src="https://cdn.pixabay.com/photo/2012/04/13/11/16/pine-31913_640.png"/> 
+//             <input id={id} name={name} type='checkbox' checked={checkedState[index]} onChange={() => handleOnChange(index)} />
+//         </div>
+//     )
+// // onClick={event => setChecked(event.target.checked)}
+//     // } else {
+
+//     //     return (
+//     //         <div className='park-container'> 
+//     //         <p>{name}</p>
+//     //         <img className='tracker-image' src="https://images.vexels.com/media/users/3/136584/isolated/preview/96086ee49328e64825862a853297d4e2-pine-tree-silhouette.png"/> 
+//     //         <input id={id} type='checkbox' onClick={event => setChecked(event.target.checked)} />
+//     //     </div>
+//     //     )
+//     // }
     
-}
+// }
 
-ParkTrackerCard.propTypes = {
-    name: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired
-}
+// ParkTrackerCard.propTypes = {
+//     name: PropTypes.string.isRequired,
+//     id: PropTypes.number.isRequired
+// }
