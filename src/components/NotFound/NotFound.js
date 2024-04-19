ab728e8e1e5972e7d8710ca87cfc8a67d54ebc55
Erin Kelley <erin.kelley01@gmail.com>
2024-04-18 20:35:08 -0600

    Add styling to error message component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,11 @@
+import './NotFound.css';
+
 export default function NotFound(){
     return (
         <div>
             <h1>Awww, nuts!</h1>
-            <p>404 Page Not Found: Please try again later.</p>
-            <img src="https://i.pinimg.com/736x/22/b2/f0/22b2f04cfe1fe936dc8c224deb4a49a4.jpg"/> 
+            <p className='error-message'>404 Page Not Found: Please try again later.</p>
+            <img className='squirrel' src="https://i.pinimg.com/736x/22/b2/f0/22b2f04cfe1fe936dc8c224deb4a49a4.jpg"/> 
         </div>
     )
 }
