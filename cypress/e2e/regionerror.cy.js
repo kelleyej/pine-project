92af807d1a570be33f57e81588ada47924e08fc3
Erin Kelley <erin.kelley01@gmail.com>
2024-05-04 08:54:36 -0600

    Place cypress.env.json in root directory

--- cypress/e2e/regionerror.cy.js
+++ cypress/e2e/regionerror.cy.js
@@ -1,20 +1,18 @@
 describe('Other error handling', () => {
 beforeEach(() => {
-    const openWeatherApiKey = Cypress.env('REACT_APP_API_SECRET_KEY')
-    const NPSApiKey = Cypress.env('REACT_APP_API_KEY')
     cy.intercept('GET', 'https://national-park-ce3a01ee1a6f.herokuapp.com/api/v1/locations', {
         statusCode: 500, 
         body: {
             message: "Internal Server Error"
         }
       }).as('serverError'),
-    cy.intercept('GET', `https://developer.nps.gov/api/v1/parks/?api_key=${NPSApiKey}&parkCode=crla&limit=62`, {
+    cy.intercept('GET', `https://developer.nps.gov/api/v1/parks/?api_key=${Cypress.env('env').REACT_APP_API_KEY}&parkCode=crla&limit=62`, {
         statusCode: 500, 
         body: {
             message: "Internal Server Error"
         }
   }).as('serverError'),
-  cy.intercept('GET', `https://api.openweathermap.org/data/2.5/weather?lat=42.94065854&lon=-122.1338414&appid=${openWeatherApiKey}&units=imperial`, {
+  cy.intercept('GET', `https://api.openweathermap.org/data/2.5/weather?lat=42.94065854&lon=-122.1338414&appid=${Cypress.env('env').REACT_APP_API_SECRET_KEY}&units=imperial`, {
       statusCode: 500, 
       body: {
         message: "Internal Server Error"
@@ -26,13 +24,13 @@ beforeEach(() => {
             message: "Bad Request"
         }
   }).as('badRequest'),
-    cy.intercept('GET', `https://developer.nps.gov/api/v1/parks/?api_key=${NPSApiKey}&parkCode=crla&limit=62`, {
+    cy.intercept('GET', `https://developer.nps.gov/api/v1/parks/?api_key=${Cypress.env('env').REACT_APP_API_KEY}&parkCode=crla&limit=62`, {
         statusCode: 404, 
         body: {
             message: "Bad Request"
         }
     }).as('badRequest'),
-    cy.intercept('GET', `https://api.openweathermap.org/data/2.5/weather?lat=42.94065854&lon=-122.1338414&appid=${openWeatherApiKey}&units=imperial`, {
+    cy.intercept('GET', `https://api.openweathermap.org/data/2.5/weather?lat=42.94065854&lon=-122.1338414&appid=${Cypress.env('env').REACT_APP_API_SECRET_KEY}&units=imperial`, {
         statusCode: 404, 
         body: {
             message: "Bad Request"
